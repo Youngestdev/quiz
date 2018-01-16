c1a4eb945349ea3624bb80bc570259f6d3695420
Abdulazeez Abdulazeez Adeshina <laisibizness@gmail.com>
2018-01-16 16:51:13 +0100

    Small jQuery code to handle checkbox

--- public/js/script.js
+++ public/js/script.js
@@ -9,7 +9,37 @@ $("input:checkbox").on('click', function() {
       $box.prop("checked", false);
     }
   });
-  
+
+  // Jquery to handle checkbox values. Hopefully it works..
+  $(document).ready(function () {
+	/* Get the checkboxes values based on the class attached to each check box */
+	$("#submit").click(function() {
+	    getValueUsingClass();
+	});
+});
+
+function getValueUsingClass(){
+	/* declare an checkbox array */
+	var chkArray = [];
+	/* look for all checkboes that have a class 'answer' attached to it and check if it was checked */
+	$(".answer:checked").each(function() {
+		chkArray.push($(this).val());
+	});
+	
+	/* we join the array separated by the comma */
+	var selected;
+	selected = chkArray.join(',') ;
+	
+	/* check if there is selected checkboxes, by default the length is 1 as it contains one single comma */
+	if(selected.length > 1){
+        console.log(selected);
+        // alert("You have selected " + selected);	
+	}else{
+		alert("Please check a box");	
+	}
+}
+
+
   // Handles the feri-feri for the start application
 document.getElementById("start").addEventListener("click", destroy);
     function destroy(){
